7ce95edf5f2fc63f5bfb018a1c12795c9b53fe19
Pragades-Rajagopal <pragadesgoldsmith@outlook.com>
2022-06-26 00:09:15 +0530

    commit-6

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.db.models import Q
+from books.models import Book
 
-# Create your views here.
+def search (request):
+    query = request.GET.get('q', '')
+    if query:
+        qset = (
+            Q(title__contains=query) |
+            Q(authors__first_name__contains=query) |
+            Q(authors__last_name__contains=query)
+        )
+        results = Book.objects.filter(qset).distinct()
+    else:
+        results = []
+
+    return render(request, 'books/search.html', {
+        'results': results,
+        'query': query
+    })
