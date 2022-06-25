540632257b6b1d48c3bd03a8aa9692a978b3cbe7
Pragades Rajagopal <66718413+Pragades-Rajagopal@users.noreply.github.com>
2022-06-25 10:36:32 +0000

    codespace-test-commit-4

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mysite.views import current_datetime
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('time/', current_datetime),
 ]
