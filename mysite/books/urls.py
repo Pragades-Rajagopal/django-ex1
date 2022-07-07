a4b9997b116fb71175534398d927b014678cec9a
Pragades-Rajagopal <pragadesgoldsmith@outlook.com>
2022-07-07 22:54:40 +0530

    commit-9

--- mysite/books/urls.py
+++ mysite/books/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('about/', views.about_pages, name='about_pages'),
     path('publishers/', views.PulisherListView.as_view(), name='publisher_list'),
-    path('publishers/<int:pub_id>', views.get_publisher, name='get_publisher')
+    path('publishers/<int:pub_id>', views.get_publisher, name='get_publisher'),
+    path('hello/', views.generate_pdf),
+    path('csv/', views.generate_csv)
 ]
 
--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.core.mail import send_mail
 from django.db.models.query_utils import Q
 from django.template import TemplateDoesNotExist
 from django.views.generic import ListView
 from books.models import Book, Publisher
 from books.forms import ContactForm
+from reportlab.pdfgen import canvas
+import csv
 
 def search (request):
     query = request.GET.get('q', '')
@@ -59,4 +61,29 @@ def get_publisher(request, pub_id):
     publisher = get_object_or_404(Publisher, pk=pub_id)
     books = Book.objects.filter(publisher_id=publisher.id)
     return render(request, 'books/publisher_detail.html', {'publisher': publisher, 'books': books})
+
+
+def generate_pdf (request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename=hello.pdf'
+
+    p = canvas.Canvas(response)
+    p.drawString(100, 100, "Hello World.")
+    p.showPage()
+    p.save()
+    return response
+
+
+UNRULY_PASSENGERS = [146,184,235,200,226,251,299,273,281,304,203]
+def generate_csv (request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachement; filename=passenger.csv'
+
+    writer = csv.writer(response)
+    writer.writerow(['Year', 'No of Passengers'])
+    for (year, num) in zip(range(1995, 2006), UNRULY_PASSENGERS):
+        writer.writerow([year, num])
+    
+    return response
+
 
