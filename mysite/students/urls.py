c81160f13112e4e1a9113afc8e7f6283a81aa9df
Pragades-Rajagopal <pragadesgoldsmith@outlook.com>
2022-07-13 23:23:26 +0530

    commit-12

--- mysite/students/urls.py
+++ mysite/students/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('hello/', views.hello),
     path('list/', views.student_list.as_view(), name='student_list'),
     path('marksheet/<int:id>', views.marksheet, name='marksheet'),
-    path('marksheet/<int:id>', views.marksheet, name='get_marks')
+    path('marksheet/<int:id>', views.marksheet, name='get_marks'),
+    path('page/', views.student_page, name='student_page')
 ]
 
--- mysite/students/views.py
+++ mysite/students/views.py
@@ -20,27 +20,39 @@ def marksheet(request, id):
         t = Marksheet.objects.filter(student_id = student.id).values('student_id').annotate(total=Sum('mark'))
         a = Marksheet.objects.filter(student_id = student.id).values('student_id').annotate(avg=Round(Avg(('mark'))))
         
-        mark_list = []
-
-        for i in range(0, mark.count()):
-            mark_list.append({'subject': mark[i]})
-        
-        print(mark_list)
+        # mark_list = [{'subject': mark[i]} for i in range(0, mark.count())]
 
         total = t[0]['total']
         avg = a[0]['avg']
         
         # return HttpResponse(json.dumps({'total': total, 'avg': avg}), content_type="application/json")
-        return render(request, 'students/student_mark.html', {'total': total, 'avg': avg, 'student': student.name, 'marks': mark})
+        return render(request, 'students/student_mark.html', {'total': total, 'avg': avg, 'student': student, 'marks': mark})
 
     except (IndexError):
-        return render(request, 'students/student_mark.html', {'student': student.name, 'message': 'Marksheet not available'})
+        return render(request, 'students/student_mark.html', {'student': student, 'message': 'Marksheet not available'})
 
 
 
 class student_list(ListView):
     model = Student
     context_object_name = 'student_list'
+
+def student_page(request):
+    try:
+        student = Student.objects.all().order_by('-grade', 'name')
+        mark_list = [
+            {
+                'student': student[i], 
+                'mark_details': Marksheet.objects.filter(student_id= student[i].id),
+                'total': Marksheet.objects.filter(student_id = student[i].id).values('student_id').annotate(total=Sum('mark'))
+            }
+            for i in range(0, student.count())
+        ]
+        # print(mark_list)
+        
+        return render(request, 'students/student_page.html', {'mark_list': mark_list})
+    except:
+        pass
 
 
 
